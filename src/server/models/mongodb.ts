392b71d05cad49fcf12252999f1ff887961ba93c
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-04-19 13:59:03 -0500

    Add starter project

---
+++ src/server/models/mongodb.ts
@@ -0,0 +1,6 @@
+import mongoose = require("mongoose");
+import { dbHost, dbName, dbUser, dbPass } from "../config";
+
+export const db = mongoose.connect(`mongodb://${dbHost}/${dbName}`,
+                          { user: dbUser,
+                            pass: dbPass });
