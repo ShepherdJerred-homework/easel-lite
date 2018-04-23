5c2c4f9db01e03bde7f882c7e9de44f9aeb016fb
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-04-23 12:08:33 -0500

    Remove handlebars

--- src/server/app/index.ts
+++ src/server/app/index.ts
@@ -1,5 +1,4 @@
 import express = require('express');
-import expressHandlebars = require('express-handlebars');
 import morgan = require('morgan');
 import bodyParser = require('body-parser');
 import expressSession = require('express-session');
@@ -8,26 +7,15 @@ import * as config from '../config';
 import * as api from './api';
 
 export const app: express.Express = express();
+const FileStore = sessionFileStore(expressSession);
 
-// Configure handlebars engine
-app.engine('hb', expressHandlebars({
-  extname: '.hb',
-  partialsDir: 'src/server/views/partials'
-}));
-app.set('views', 'src/server/views');
-app.set('view engine', 'hb');
-
-// Logging
 if (config.logFormat) {
   app.use(morgan(config.logFormat));
 }
 
-// POST data
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Session
-const FileStore = sessionFileStore(expressSession);
 app.use(expressSession({
   secret: config.sessionSecret,
   store: new FileStore(),
@@ -35,15 +23,7 @@ app.use(expressSession({
   resave: false
 }));
 
-// Custom Middleware
 app.use('/api/', api.router);
 app.use('/el/', express.static('./lib/client'));
 
-// Static files
 app.use(express.static('./public'));
-
-// Last resort
-app.use((req, res) => {
-  res.status(404);
-  res.render('error404', req);
-});
