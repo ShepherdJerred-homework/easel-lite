996cf908aa6110cec11857c8dbaec5e38827c209
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-04-23 11:48:39 -0500

    Add routes

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -1,10 +1,46 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClassListComponent } from './class-list/class-list.component';
+import { ClassDetailsComponent } from './class-details/class-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ClassRosterComponent } from './class-roster/class-roster.component';
+import { CreateClassComponent } from './create-class/create-class.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'classes/:name',
+    component: ClassDetailsComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'classes',
+    component: ClassListComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'rosters/:name',
+    component: ClassRosterComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'new-class',
+    component: CreateClassComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    redirectTo: 'classes',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
