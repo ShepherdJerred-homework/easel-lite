a1d9a142a783ff1f5a8eb56d19434fe5d38f9fec
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-04-24 11:05:30 -0500

    Remove equals method

--- src/client/app/models/Class.ts
+++ src/client/app/models/Class.ts
@@ -6,12 +6,4 @@ export class Class {
   number: number;
   title: string;
   teacher: User;
-
-  static equals (r: Class, l: Class): boolean {
-    return l.department === r.department &&
-      l.id === r.id &&
-      l.number === r.number &&
-      l.title === r.title &&
-      l.teacher.id === r.teacher.id;
-  }
 }
