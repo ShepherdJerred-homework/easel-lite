746756cb24a524337630654437e5edde5428ecec
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-04-24 13:38:17 -0500

    Add method for creating class

--- src/client/app/services/class.service.ts
+++ src/client/app/services/class.service.ts
@@ -18,6 +18,10 @@ export class ClassService {
     return this.http.get<Class>('/api/classes/' + name);
   }
 
+  createClass (classs: Class): Observable<Class> {
+    return this.http.post('/api/classes/', classs) as Observable<Class>;
+  }
+
   updateClass (classs: Class): Observable<Class> {
     let teacher = classs.teacher as User;
     classs.teacher = teacher.id;
