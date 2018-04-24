7647e4533f61559ec7b1a2c6aa15f290185bb9ee
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-04-24 13:13:52 -0500

    Move navbar and container to separate components

---
+++ src/client/app/components/container/container.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'el-container',
+  templateUrl: './container.component.html',
+  styleUrls: []
+})
+export class ContainerComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
+}
